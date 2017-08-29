22377d811d901437d24f0a92e88f8b05e558c686
Austin Drenski <austin.drenski@gmail.com>
2017-08-29 10:45:16 -0400

    Add files via upload

---
+++ unit_testing_example/test_experiment.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from experiment import Experiment
+
+__author__ = "Austin Drenski"
+__project__ = "unit_testing_example"
+
+
+class TestExperiment(TestCase):
+    """
+    Unit tests for the Experiment class.
+    """
+
+    def test_evaluate_derivative(self):
+        self.fail()
+
+    def test_evaluate(self):
+        # Arrange
+        experiment = Experiment(0, 0)
+
+        # Act
+        result = experiment.evaluate(1)
+
+        # Assert
+        self.assertEqual(1, result)
