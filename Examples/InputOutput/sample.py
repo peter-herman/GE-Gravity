1f5c53c5bd0bdc1edb0a58ef93f76ad6a4a092bc
Austin Drenski <austin.drenski@gmail.com>
2017-09-11 11:41:44 -0400

    9-11-17 programming meeting - end of meeting

--- Examples/InputOutput/sample.py
+++ Examples/InputOutput/sample.py
@@ -1,10 +1,11 @@
-from Examples.InputOutput.World import *
+from Examples.InputOutput.World import read_world
 
 filePath = "\\users\\austin.drenski\\desktop\\gravity_programming_sample_data.txt"
 
 world = read_world(filePath)
 
 world.countries
+
 world.find_country("USA")
 world.find_gdp("2014")
 world.find_gdp("2015")
