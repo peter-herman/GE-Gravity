cc0047459d9c0a071cae2eeb28fb11a022e533d3
Peter Herman <peter.r.herman@gmail.com>
2017-10-03 11:09:04 -0400

    added MR solving

--- Peter_GEGM_model_development/estimation_and_mr_terms_v1.py
+++ Peter_GEGM_model_development/estimation_and_mr_terms_v1.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+import scipy.optimize as spo
 
 # read in data
 grav_data = pd.read_csv("\\\\hq-fs-1.cloudnet.usitc.gov\\FS Econ only\\data\\Gravity Resources\\GEPPMLwithMatlab\\working\\Peter\\ge_ppml_data.csv") #read in csv as DataFrame
@@ -68,7 +69,75 @@ for cost in cfl_vars: # for each of the included costs from before
 grav_data['t_ij_cfl'] = np.exp(grav_data['t_ij_cfl']) # t_ij = exp(costs)
 
 
+##########################
+## Prep Data For Solver ##
+##########################
+
+## Trade Costs
 # Reformat Wide
 trade_costs = grav_data[['importer','exporter','t_ij_bsln']]
-trade_costs['t_exp_'] = trade_costs.groupby('exporter').cumcount()
-trade_costs_wide = trade_costs.pivot('importer', 'exporter', 't_ij_bsln')
+#trade_costs['t_exp_'] = trade_costs.groupby('exporter').cumcount()
+trade_costs_imp = trade_costs.pivot('importer', 'exporter', 't_ij_bsln')
+
+
+## Country stats (output, expenditure, phi)
+
+
+
+country_stats = grav_data[['importer', 'exporter','output','expndr']].sort_values(by = 'importer')
+country_stats = country_stats[(country_stats.importer == country_stats.exporter)]
+country_stats['phi'] = country_stats.expndr/country_stats.output
+
+#set index as country names so that df.loc[] can be used to call specific entries
+country_stats.set_index('importer', inplace = True, drop = False)
+country_list = country_stats['importer']
+
+#calculate output shares
+total_output = sum(country_stats['output'])
+country_stats['output_share'] = country_stats['output']/total_output
+
+#calculate expenditure shares
+total_expenditure = sum(country_stats['expndr'])
+country_stats['expend_share'] = country_stats['expndr']/total_expenditure
+
+
+
+###
+#Set up MRS solver
+####
+mrs = pd.DataFrame(country_stats.importer)
+mrs['mrs_inward'] = 1
+mrs['mrs_outward'] = 1
+mrs.set_index('importer', drop = True, inplace = True)
+country_info = country_stats
+
+def mrs_functions(mrs, trade_costs, country_info,country_list):
+    mrs_slack = np.zeros(2*len(country_info.importer))  # create list of zeros
+    for i in range(len(country_info.importer)-1):
+        imp_index = country_list.iloc[i] #select a country for the current iteration for an index location
+        cost_calculation = 0
+        for k in range(len(country_info.importer)):
+            exp_index = country_list.iloc[k]
+            cost_calculation += trade_costs_imp.loc[imp_index, exp_index] * \
+                    country_info.output_share.loc[exp_index] * \
+                    mrs.mrs_outward.loc[exp_index]
+        mrs_slack[i] = 1 - mrs.mrs_inward.loc[imp_index] * cost_calculation
+
+    for j in range(len(country_info.importer)):
+        exp_index = country_list.iloc[j]
+        cost_calculation = 0
+        for k in range(len(country_info.importer)):
+            imp_index = country_list.iloc[k]
+            cost_calculation += trade_costs_imp.loc[imp_index,exp_index] *\
+                    country_info.expend_share.loc[imp_index] * \
+                    mrs.mrs_inward.loc[imp_index]
+        mrs_slack[j + len(country_info.importer)] = 1 - mrs.mrs_outward.loc[exp_index] * cost_calculation
+    return mrs_slack
+
+
+mrs_functions(mrs,trade_costs_imp,country_stats, country_list)
+
+feeder = lambda mrs_0: mrs_functions(mrs_0, trade_costs_imp,country_stats, country_list)
+
+solution_1 = spo.root(feeder, mrs)
+#It does not seem to like having a dataframe fed in as an optimization arguement. Try convertin mrs to a simple vector.
