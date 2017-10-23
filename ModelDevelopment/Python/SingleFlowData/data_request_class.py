ca361d8113f199ceccc0711ca6e93ee199771907
Peter Herman <peter.r.herman@gmail.com>
2017-10-23 15:55:14 -0400

    added documentation

--- ModelDevelopment/Python/SingleFlowData/data_request_class.py
+++ ModelDevelopment/Python/SingleFlowData/data_request_class.py
@@ -8,6 +8,9 @@ __version__ = "1.0.0"
 from typing import List
 
 class single_flow_data_request(object):
+    """
+    Object for storing the specifications for a USITC data api request
+    """
     def __init__(self,
                  importers: List,
                  exporters: List,
@@ -19,6 +22,18 @@ class single_flow_data_request(object):
                  iso3: str = 'True',
                  aggregation: str = ""
                  ):
+        """
+
+        :param importers: a list of importer Iso3s
+        :param exporters: a list of exporter Iso3s
+        :param years: a list of years
+        :param source: a string specifying data source ('comtrade', 'dataweb')
+        :param frequency: frequency of observation ('annual')
+        :param nomenclature: product nomenclature ('hs6')
+        :param file_format: file format to return ('csv', 'json')
+        :param iso3: boolean for inclusion of ISO3 codes
+        :param aggregation: level of aggregation ('hs2', 'hs4', 'hs6')
+        """
         self.importers = importers
         self.exporters = exporters
         self.years = years
