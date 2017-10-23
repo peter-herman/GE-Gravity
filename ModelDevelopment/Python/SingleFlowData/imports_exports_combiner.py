66965d22079f7126f406c0b5a6765eca1eefbd5a
Peter Herman <peter.r.herman@gmail.com>
2017-10-23 14:52:45 -0400

    trade flows combined via average using pandas and USITC data API

--- ModelDevelopment/Python/SingleFlowData/imports_exports_combiner.py
+++ ModelDevelopment/Python/SingleFlowData/imports_exports_combiner.py
@@ -1,4 +1,4 @@
-import requests
+import pandas as pd
 from get_data_query_builder import *
 
 imports_request_url = get_data_query_builder(years = (2013,2014,2015),
@@ -19,20 +19,24 @@ exports_request_url = get_data_query_builder(years = (2013,2014,2015),
 
 
 
-response_imports = requests.get(imports_request_url)
-response_exports = requests.get(exports_request_url)
-
-imports_data = response_imports.json()
-type(imports_data[1])
-imports_data[1]
 
 
+type(response_imports.text)
 
-def rekey_comtrade(dict_list, old_key, new_key):
-    for key in range(len(dict_list)):
-        dict_list[key][new_key] = dict_list.pop[key](old_key)
+imports_data = pd.read_json(imports_request_url)
+exports_data = pd.read_json(exports_request_url)
+
+imports_data.rename(columns = {'reporterIso3':'importer', 'partnerIso3':'exporter',
+                               'costBasis': 'costBasis_imports', 'tradeFlow':'trade_flow_imports',
+                               'tradeValue':'trade_value_imports'}, inplace = True)
+exports_data.rename(columns = {'reporterIso3':'exporter', 'partnerIso3':'importer',
+                               'costBasis': 'costBasis_exports',
+                               'tradeFlow':'trade_flow_exports',
+                               'tradeValue': 'trade_value_exports'}, inplace = True)
+
+merged_data = imports_data.merge(exports_data, how = 'outer', on = ('importer', 'exporter', 'year', 'productCode'))
+merged_data['trade_value_exports'].fillna(merged_data['trade_value_imports'], inplace = True)
+merged_data['trade_value_imports'].fillna(merged_data['trade_value_exports'], inplace = True)
+merged_data['single_flow'] = (merged_data['trade_value_exports'] + merged_data['trade_value_imports'])/2
 
 
-temp = rekey_comtrade(imports_data, 'reporter', 'importer')
-
-imports_data[1]['reporterIso3']
