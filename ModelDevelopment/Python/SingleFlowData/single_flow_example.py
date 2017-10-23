9c6726bb480078d913d4ec98f4fc2332f3593978
Peter Herman <peter.r.herman@gmail.com>
2017-10-23 15:57:55 -0400

    added documentation

--- ModelDevelopment/Python/SingleFlowData/single_flow_example.py
+++ ModelDevelopment/Python/SingleFlowData/single_flow_example.py
@@ -1,6 +1,13 @@
 
+__author__ = "Peter Herman"
+__project__ = "GE-Gravity.ModelDevelopment.Python.SingleFlowData"
+__created__ = "10-23-2017"
+__altered__ = "10-23-2017"
+__version__ = "1.0.0"
 
-
+"""
+Example for how to request data and construct a single flow column
+"""
 
 from ModelDevelopment.Python.SingleFlowData.data_request_class import single_flow_data_request
 from ModelDevelopment.Python.SingleFlowData.single_flow_constructor import single_flow_constructor
