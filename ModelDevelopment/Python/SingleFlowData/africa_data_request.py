21d9a191d8c4dc44d486a72357f2b641d7ac7163
Peter Herman <peter.r.herman@gmail.com>
2017-11-07 13:37:00 -0500

    minor name change of written csv file

--- ModelDevelopment/Python/SingleFlowData/africa_data_request.py
+++ ModelDevelopment/Python/SingleFlowData/africa_data_request.py
@@ -10,14 +10,14 @@ Example for how to request data and construct a single flow column
 from ModelDevelopment.Python.SingleFlowData.data_request_class import single_flow_data_request
 from ModelDevelopment.Python.SingleFlowData.single_flow_constructor import single_flow_constructor
 
-output_csv_file_name = "africa_comtrade_data_single_flow_hs2agg.csv"
+output_csv_file_name = "africa_comtrade_data_single_flow_hs6agg_fewer_columns.csv"
 save_location = "\\\\hq-fs-1.cloudnet.usitc.gov\\FS Econ only\\data\\Gravity Resources\\Data\\Africa Data\\"
 
 request_africa = single_flow_data_request(years=[2013, 2014, 2015],
                                         importers=["all"],
                                         exporters=["all"],
                                         source='comtrade',
-                                        aggregation='hs2',
+                                        aggregation='hs6',
                                         file_format='json')
 
 africa_data = single_flow_constructor(request_africa)
